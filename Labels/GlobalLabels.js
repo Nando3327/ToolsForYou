2ebb5183d45ff6b1b49e506848c4579f30d45c8b
Efestor <48194515+Efestor@users.noreply.github.com>
2019-03-03 22:41:27 -0500

    Add files via upload

---
+++ Labels/GlobalLabels.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Fernando on 03/03/19.
+ */
+
+
+var defaultLabels = {
+    inputs: {
+        dateSince: 'Fecha desde',
+        dateFor: 'Fecha Hasta',
+    },
+    buttons: {
+        search: 'Consultar'
+    }
+};
+export default defaultLabels;
