9767cf39ffe66b1e1238cabfa8607f9903cf52ab
Fernando Castillo <fernando.castillo@easysoft.com.ec>
2019-09-13 18:54:28 -0500

    Prueba Routes 1.0

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import LoginForm from './Components/FormLogin/FormLogin'
-import RegisterForm from './Components/FormRegister/FormRegister'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {faIgloo} from '@fortawesome/free-solid-svg-icons'
-import GlobalLabels from './Labels/GlobalLabels'
 import Routers from './Components/Routers/Routers'
 
 library.add(faIgloo);
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            showLogin: true,
-        };
-    }
-
-    register = (event) => {
-        this.setState({
-            showLogin: !this.state.showLogin,
-        })
-    };
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <div className="container">
-                        <img src={logo} className="App-logo" alt="logo"/>
-                        <div id="ContentShow">
-                            <div className="row">
-                                <Routers />
-                            </div>
-                        </div>
-                    </div>
-                </header>
-            </div>
+            <Routers />
         );
     }
 }
--- src/Components/Routers/Routers.js
+++ src/Components/Routers/Routers.js
@@ -7,24 +7,48 @@ import GlobalLabels from '../../Labels/GlobalLabels'
 import RegisterForm from '../FormRegister/FormRegister'
 import LoginForm from '../FormLogin/FormLogin'
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import logo from '../../logo.svg';
 
 
 class Routers extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showLogin: true,
+        };
+    }
+    register = (event) => {
+        this.setState({
+            showLogin: !this.state.showLogin,
+        })
+    };
     render() {
         return (
             <Router>
-                <div>
-                    <ul>
-                        <li>
-                            <Link to="/">{labels.tittles.login}</Link>
-                        </li>
-                        <li>
-                            <Link to="/Register">{labels.tittles.register}</Link>
-                        </li>
-                    </ul>
+                <div className="App">
+                    <header className="App-header">
+                        <div className="container">
+                            <img src={logo} className="App-logo" alt="logo"/>
+                            <div id="ContentShow">
+                                <div className="row">
+                                    <div>
+                                        <ul>
+                                            <li>
+                                                <Link to="/">{labels.tittles.login}</Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/Register">{labels.tittles.register}</Link>
+                                            </li>
+                                        </ul>
 
-                    <Route exact path="/" component={LoginForm}/>
-                    <Route exact path="/Register" component={RegisterForm}/>
+                                        <Route exact path="/" component={LoginForm}/>
+                                        <Route exact path="/Register" component={RegisterForm}/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </header>
                 </div>
             </Router>
         );
